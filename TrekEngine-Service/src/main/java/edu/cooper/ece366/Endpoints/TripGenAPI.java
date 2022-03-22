0df3e7a515e1f8c78030c5ee3319cff96592e698
gkgkgkgk <gkepets@gmail.com>
2022-03-22 16:16:45 -0400

    added endpoint

--- TrekEngine-Service/src/main/java/edu/cooper/ece366/Endpoints/TripGenAPI.java
+++ TrekEngine-Service/src/main/java/edu/cooper/ece366/Endpoints/TripGenAPI.java
@@ -3,10 +3,17 @@ package edu.cooper.ece366.Endpoints;
 import static spark.Spark.*;
 
 public class TripGenAPI {
+
+    static String GetRelevantTrips() {
+        // This function will eventually get a mix of relevant Trips from friends,
+        // popular users, and auto generated trips.
+        return "trips trips blah blah blah";
+    }
+
     public static void paths() {
         // login post request authenticator and returns a user object to the client
         path("/tripgen", () -> {
-            post("/submit", (req, res) -> "Success");
+            post("/submit", (req, res) -> GetRelevantTrips());
         });
     }
 }
