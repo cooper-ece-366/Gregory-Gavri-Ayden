fb9e29817c6d4a5f20299ee7e92eb2a0da298947
Shankface <aydenshankman1@gmail.com>
2022-04-12 15:41:54 -0400

    fixed

--- datagen/datagen2.js
+++ datagen/datagen2.js
@@ -35,43 +35,37 @@ const insert = (obj)=>{
 }
 
 const getTag = (p_type)=>{
-    if (p_type == 'natural_feature' || p_type == 'park') return 'nature';
+    if (p_type == 'natural_feature' || p_type == 'park' || p_type == 'national_park') return 'nature';
     if (p_type == 'museum' || p_type == 'art_gallery') return 'landmark';
     if (p_type == 'rv_park' || p_type == 'campground') return 'lodging';
     if (p_type == 'casino' || p_type == 'zoo' || p_type == 'tourist_attraction' || p_type == 'stadium' || p_type == 'night_club' || p_type == 'amusement_park') return 'entertainment';
     return p_type;
 }
 
+const getStop = (obj,tag1)=>{
+    const stop = {
+        name: obj.name,
+        lat: obj.lat,
+        lng: obj.lng,
+        g_types: obj.types,
+        tag: getTag(tag1)
+    }
+    return stop;
+}
+
 const generateStops = async (obj)=>{
     
-    for (let i = 0; i < 2; i++) {
-        console.log(obj[i].name);
-        const {d} = await search(obj[i].name); 
-        city_park_stop = {
-            name: obj[i].name,
-            lat: obj[i].lat,
-            lng: obj[i].lng,
-            g_types: d.types,
-            tag: obj[i].tag
-        }
-        insert(city_park_stop);
-        console.log(city_park_stop);
+    for (let i = 0; i < 1; i++) {
+        const {data} = await search(obj[i].name); 
+        insert(getStop(data, obj[i].type));
+
         let latlng = obj[i].lat.toString().concat(" ").concat(obj[i].lng.toString());
 
         for(let j = 0; j < p_types.length; j++){
             const {data} = await nearby(latlng, p_types[j],"50000"); 
 
             for(let k = 0; k < data.length; k++){
-
-                
-                let stop = {
-                    name: data[k].name,
-                    lat: data[k].lat,
-                    lng: data[k].lng,
-                    g_types: data[k].types,
-                    tag: getTag(p_types[j])
-                }
-                insert(stop); 
+                insert(getStop(data[k],p_types[j])); 
             }
         }
     }
