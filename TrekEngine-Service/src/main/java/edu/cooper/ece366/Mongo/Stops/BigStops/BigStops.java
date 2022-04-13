d2ad02475c7a2a5bcb6b983287d65b8cf7836790
Gregory Presser <gpress2222@gmail.com>
2022-04-13 17:44:20 -0400

    First start and restructuring2

--- TrekEngine-Service/src/main/java/edu/cooper/ece366/Mongo/Stops/BigStops/BigStops.java
+++ TrekEngine-Service/src/main/java/edu/cooper/ece366/Mongo/Stops/BigStops/BigStops.java
@@ -1,7 +1,20 @@
 package edu.cooper.ece366.Mongo.Stops.BigStops;
 
-import edu.cooper.ece366.Mongo.Stops.Location; 
+import org.bson.codecs.pojo.annotations.BsonCreator;
+import org.bson.codecs.pojo.annotations.BsonProperty;
+import org.bson.types.ObjectId;
 
-public class BigStops extends Location {
-    
+import edu.cooper.ece366.Mongo.Stops.Loacation;
+
+public class BigStops extends Loacation {
+
+    @BsonCreator
+    public BigStops(
+        @BsonProperty("_id") ObjectId id,
+        @BsonProperty("name") String name,
+        @BsonProperty("lat") Double lat,
+        @BsonProperty("lng") Double lng,
+        @BsonProperty("type") String type){
+            super(id,name,lat,lng,type); 
+        }
 }
